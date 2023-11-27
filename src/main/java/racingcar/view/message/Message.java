4ba3afa609b9ad9b5d7ba4f6ace2dab9b5e96ae2
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-28 01:39:48 +0900

    [style] 메시지 내용 작성

---
+++ src/main/java/racingcar/view/message/Message.java
@@ -0,0 +1,7 @@
+package racingcar.view.message;
+
+public class Message {
+    public static final String CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String TRY = "시도할 회수는 몇회인가요?";
+    public static final String WINNER = "최종 우승자 : ";
+}
