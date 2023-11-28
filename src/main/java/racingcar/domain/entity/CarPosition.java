96eed18d5bc9a210020da74ba307159251677356
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-28 20:36:46 +0900

    [refactoring] 객체 단일 책임 강화

--- src/main/java/racingcar/domain/entity/CarPosition.java
+++ src/main/java/racingcar/domain/entity/CarPosition.java
@@ -2,26 +2,24 @@ package racingcar.domain.entity;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RacingCar extends Car{
+public class CarPosition extends Car{
     private final int FORWARD = 1;
-    private final int MOVE_STANDARD = 4;
 
     private int position = 0;
 
-    public RacingCar(String name) {
+    public CarPosition(String name) {
         super(name);
         this.position = incrementPosition();
     }
 
     public int incrementPosition() {
-        if (isMove()) {
+        if (super.isMove()) {
             this.position += FORWARD;
         }
         return this.position;
     } // 외부에서 얘를 계속 호출해야 됨
 
-    private boolean isMove() {
-        int move = Randoms.pickNumberInRange(0,9);
-        return move >= MOVE_STANDARD;
+    private int comparePosition(CarPosition other) {
+        return this.position - other.position;
     }
 }
