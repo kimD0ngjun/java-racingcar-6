5b39808afbfb00f8ad0a7224e643fd737afa7125
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-28 21:54:57 +0900

    [feat] 자동차 객체 위치 기반 트랙 작성 기능

--- src/main/java/racingcar/domain/entity/CarPosition.java
+++ src/main/java/racingcar/domain/entity/CarPosition.java
@@ -2,6 +2,7 @@ package racingcar.domain.entity;
 
 public class CarPosition extends Car{
     private final int FORWARD = 1;
+    private final String TRACK = "-";
 
     private int position = 0;
 
@@ -17,7 +18,19 @@ public class CarPosition extends Car{
         return this.position;
     } // 외부에서 얘를 계속 호출해야 됨
 
-    private int comparePosition(CarPosition other) {
+    // 1등 정하기
+    public int comparePosition(CarPosition other) {
         return this.position - other.position;
+    }
+
+    // 트랙 그리기
+    public String trackPosition() {
+        StringBuilder track = new StringBuilder();
+
+        for (int i = 0; i < this.position; i++) {
+            track.append(TRACK);
+        }
+
+        return track.toString();
     }
 }
